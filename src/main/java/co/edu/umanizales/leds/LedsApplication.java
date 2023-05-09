c20e01421392c9389b554625eda6ae5287b58f25
santi <scg172004@gmail.com>
2023-05-08 20:05:17 -0500

    proyecto leds

---
+++ src/main/java/co/edu/umanizales/leds/LedsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.leds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/umanizales/leds/LedsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.leds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
