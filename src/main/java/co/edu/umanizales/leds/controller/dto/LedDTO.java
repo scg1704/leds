c1e1c2221fc854dfdadcab96eb6d5a78fc00a550
santi <scg172004@gmail.com>
2023-05-15 14:45:20 -0500

    otra correción

---
+++ src/main/java/co/edu/umanizales/leds/controller/dto/LedDTO.java
@@ -0,0 +1,10 @@
+package co.edu.umanizales.leds.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LedDTO {
+    private int id;
+}
